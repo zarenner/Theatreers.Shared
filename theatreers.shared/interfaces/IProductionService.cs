c4913b17253809b5fc56ded982fedc38a3545335
Christian Reddington <christian.reddington@microsoft.com>
2018-11-13 04:16:39 -0800

    Remove unecessary usings, and fix dependency on shared rather than model

--- theatreers.shared/interfaces/IProductionService.cs
+++ theatreers.shared/interfaces/IProductionService.cs
@@ -1,4 +1,3 @@
-using LibraryManagement.Data.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- theatreers.shared/services/ShowService.cs
+++ theatreers.shared/services/ShowService.cs
@@ -1,9 +1,8 @@
-using LibraryManagement.Data.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using theatreers.shared.Models;
+using theatreers.shared;
 
 namespace theatreers.shared.Services
 {
